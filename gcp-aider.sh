dd63f8976ffcc5899c226925d011d1e2218cf998
Trent Zock-Robbins <robbintt@gmail.com>
2025-08-01 15:59:55 -0700

    Show thinking in gemini 2.5 pro and flash.

--- gcp-aider.sh
+++ gcp-aider.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
 MODEL=gemini/gemini-2.5-pro
-#EDITOR_MODEL=google/gemini-2.5-flash
 
-
-# These notes are only in this file:
-#
-#  --model $MODEL \
-#  --edit-format architect \
-#  --editor-model $EDITOR_MODEL \
-#  --editor-edit-format whole \
-
-# input color doesnt work with dark mode
-#  --user-input-color "#eb6521" \
-#
-# this is useful sometimes
+# these are useful sometimes
 #  --thinking-tokens 128 \
-
 #  --restore-chat-history \
-
-# should no longer be needed.
-#  --model-settings-file scripts/model.settings.yml \
-
-# need to turn off when modifying with project.yml (mostly creating new files, some devops)
 #  --yes-always \ 
+# --use-jinja2-templates \
+
+# install experimental aider via venv for now
+. ~/virtualenvs/aider_dev/bin/activate
+
 aider \
-  --model $MODEL \
-  --model-settings-file ~/model.settings.yml
+  --model $MODEL
