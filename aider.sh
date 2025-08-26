d11470af651c1df7488b7d79d5a6427c0a985896
Trent Zock-Robbins <robbintt@gmail.com>
2025-08-26 14:00:56 -0700

    Add handler example for my own use.

--- aider.sh
+++ aider.sh
@@ -16,6 +16,6 @@
 # stock aider (when i break things)
 #. ~/virtualenvs/stock_aider/bin/activate
 
-#aider --handlers file-adder --handlers potato-handler --controller-model gemini/gemini-2.5-flash
+#aider --handlers "{'name': 'file-adder', 'config': {'reflections': 6}}" --controller-model gemini/gemini-2.5-flash-lite
 
 aider
